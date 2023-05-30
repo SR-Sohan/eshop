0aba6d254a5b256fc39d6e4d294690e25b8a453c
SR-Sohan <sr.sohanur22@gmail.com>
2023-05-30 17:07:07 +0600

    after modify about contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import img from "../assets/images/about.svg"
+import React from "react";
+import img from "../assets/images/about.svg";
 
 const About = () => {
   return (
-    <div className='about_component my-5'>
+    <div className="about_component my-5">
       <div className="container">
+        <div className="about_banner">
+          <h2>About Page</h2>
+        </div>
         <div className="row">
           <div className="col-md-5">
             <div className="about_img text-center">
-              <img className='img-fluid' src={img} alt="" />
+              <img className="img-fluid" src={img} alt="" />
             </div>
           </div>
           <div className="col-md-7">
             <div className="about_content">
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita cum molestias iste velit quam eius et magni deserunt doloremque error, commodi rem vitae totam, at dolorem, temporibus sit perferendis ducimus.</p>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita cum molestias iste velit quam eius et magni deserunt doloremque error, commodi rem vitae totam, at dolorem, temporibus sit perferendis ducimus.</p>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita cum molestias iste velit quam eius et magni deserunt doloremque error, commodi rem vitae totam, at dolorem, temporibus sit perferendis ducimus.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Expedita cum molestias iste velit quam eius et magni deserunt
+                doloremque error, commodi rem vitae totam, at dolorem,
+                temporibus sit perferendis ducimus.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Expedita cum molestias iste velit quam eius et magni deserunt
+                doloremque error, commodi rem vitae totam, at dolorem,
+                temporibus sit perferendis ducimus.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Expedita cum molestias iste velit quam eius et magni deserunt
+                doloremque error, commodi rem vitae totam, at dolorem,
+                temporibus sit perferendis ducimus.
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import img from "../assets/images/contact.svg"
+import React from "react";
+import img from "../assets/images/contact.svg";
 
 const Contact = () => {
   return (
     <div className="login_component">
       <div className="container ">
+        <div className="contact_banner">
+          <h2>Contact Page</h2>
+        </div>
         <div className="row">
           <div className="col-md-6">
             <div className="form_img">
@@ -13,9 +16,20 @@ const Contact = () => {
           </div>
           <div className="col-md-6">
             <form>
-              <h3 className="text-center">Contact With Our</h3>
-             
+              <h3 className="text-center">Contact Us</h3>
+
               <hr />
+              <div className="form_input_wrap">
+                <label className="label" htmlFor="subject">
+                  Subject:
+                </label>
+                <input
+                  type="text"
+                  name="subject"
+                  id="subject"
+                  placeholder="subject"
+                />
+              </div>
               <div className="form_input_wrap">
                 <label className="label" htmlFor="email">
                   Email:
@@ -28,23 +42,24 @@ const Contact = () => {
                 />
               </div>
               <div className="form_input_wrap">
-                <label className="label" htmlFor="password">
-                  Password:
+                <label className="label" htmlFor="msg">
+                  Message:
                 </label>
                 <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="password"
+                  type="text"
+                  name="msg"
+                  id="msg"
+                  placeholder="message"
                 />
               </div>
+           
               <input className="submit_btn" type="submit" value="Send" />
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
